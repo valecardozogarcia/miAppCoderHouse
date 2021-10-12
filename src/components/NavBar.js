32cd6a56e89432266144a1f9c7c51fcb636ee884
valecardozogarcia <valecardozog@gmail.com>
2021-10-12 16:00:01 -0300

    faltaba nombre de la tienda

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
         <header className="header">
             <nav className= "navBar">
                 <img src= "../../logo.jpg" alt= "logo" className="logo"/>
+                <h1 className="brandName" >Organics</h1>
                 <a href="#" h1 className="item-nav" >Inicio</a>
                 <a href="#" h1 className="item-nav">Productos</a>
                 <a href="#" h1 className="item-nav">Contacto</a>
